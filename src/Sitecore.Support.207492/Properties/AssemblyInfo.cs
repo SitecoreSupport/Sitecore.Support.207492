b1a82f3c92782623cd646a4e734ee6cd68aa14f2
Paul Stupka <psk@sitecore.net>
2018-02-28 15:42:21 +0200

    Preparations for the fix

---
+++ src/Sitecore.Support.207492/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.207492")]
+[assembly: AssemblyProduct("Sitecore.Support.207492")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Item thumbnail is not displayed when searching in Sitecore Client using Azure Search provider. The issue is fixed by the configuration file.")]
+
